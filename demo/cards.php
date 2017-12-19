26848f1b5c2e165a7703e71a751e446b0481865a
aaa <aaa@aaa.com>
2017-12-19 23:04:58 +0200

    test ajax 0002a -ok ajax

---
+++ demo/cards.php
@@ -0,0 +1,42 @@
+<?php
+echo '
+{
+          "cards" : [
+            {"name": "Angel", "image" : "assets/top-trumps-cats-angel.jpg", "fields" : [8, 6, 5, 2]},
+            {"name": "Bastet", "image" : "assets/top-trumps-cats-bastet.jpg", "fields" : [6, 7, 4, 4]},
+            {"name": "Bull", "image" : "assets/top-trumps-cats-bull.jpg", "fields" : [8, 6, 3, 7]},
+            {"name": "Clark", "image" : "assets/top-trumps-cats-clark.jpg", "fields" : [10, 5, 2, 4]},
+            {"name": "Cuddles", "image" : "assets/top-trumps-cats-cuddles.jpg", "fields" : [7, 5, 10, 6]},
+            {"name": "Cutie", "image" : "assets/top-trumps-cats-cutie.jpg", "fields" : [7, 4, 9, 6]},
+            {"name": "Daisy", "image" : "assets/top-trumps-cats-daisy.jpg", "fields" : [2, 7, 5, 2]},
+            {"name": "Donut", "image" : "assets/top-trumps-cats-donut.jpg", "fields" : [4, 3, 3, 10]},
+            {"name": "Firefly", "image" : "assets/top-trumps-cats-firefly.jpg", "fields" : [6, 9, 4, 3]},
+            {"name": "Flabber", "image" : "assets/top-trumps-cats-flabber.jpg", "fields" : [7, 1, 6, 8]},
+            {"name": "Furry", "image" : "assets/top-trumps-cats-furry.jpg", "fields" : [7, 4, 6, 3]},
+            {"name": "Grumpy", "image" : "assets/top-trumps-cats-grumpy.jpg", "fields" : [6, 4, 1, 5]},
+            {"name": "Hunky Hank", "image" : "assets/top-trumps-cats-hunky-hank.jpg", "fields" : [6, 1, 6, 10]},
+            {"name": "Hunter", "image" : "assets/top-trumps-cats-hunter.jpg", "fields" : [5, 10, 3, 8]},
+            {"name": "Jay", "image" : "assets/top-trumps-cats-jay.jpg", "fields" : [8, 7, 5, 4]},
+            {"name": "Little Lion", "image" : "assets/top-trumps-cats-little-lion.jpg", "fields" : [4, 10, 5, 7]},
+            {"name": "Lynx", "image" : "assets/top-trumps-cats-lynx.jpg", "fields" : [3, 8, 3, 5]},
+            {"name": "Nappy", "image" : "assets/top-trumps-cats-nappy.jpg", "fields" : [5, 2, 5, 7]},
+            {"name": "Owl", "image" : "assets/top-trumps-cats-owl.jpg", "fields" : [2, 2, 1, 7]},
+            {"name": "P-Dog", "image" : "assets/top-trumps-cats-pdog.jpg", "fields" : [7, 3, 2, 9]},
+            {"name": "Precious", "image" : "assets/top-trumps-cats-precious.jpg", "fields" : [3, 7, 4, 1]},
+            {"name": "Rex", "image" : "assets/top-trumps-cats-rex.jpg", "fields" : [10, 7, 3, 7]},
+            {"name": "Rumble", "image" : "assets/top-trumps-cats-rumble.jpg", "fields" : [4, 7, 2, 7]},
+            {"name": "Scarface", "image" : "assets/top-trumps-cats-scarface.jpg", "fields" : [7, 7, 6, 6]},
+            {"name": "Scratchy", "image" : "assets/top-trumps-cats-scratchy.jpg", "fields" : [2, 4, 2, 7]},
+            {"name": "Sleeky", "image" : "assets/top-trumps-cats-sleeky.jpg", "fields" : [5, 8, 5, 4]},
+            {"name": "Spencer", "image" : "assets/top-trumps-cats-spencer.jpg", "fields" : [9, 3, 5, 6]},
+            {"name": "Swift", "image" : "assets/top-trumps-cats-swift.jpg", "fields" : [2, 9, 6, 5]},
+            {"name": "Timi", "image" : "assets/top-trumps-cats-timi.jpg", "fields" : [10, 5, 5, 4]},
+            {"name": "Tony", "image" : "assets/top-trumps-cats-tony.jpg", "fields" : [6, 6, 4, 3]},
+            {"name": "V", "image" : "assets/top-trumps-cats-v.jpg", "fields" : [1, 9, 3, 1]},
+            {"name": "Watson", "image" : "assets/top-trumps-cats-watson.jpg", "fields" : [9, 3, 4, 6]}
+          ]
+}
+
+';
+
+?>
