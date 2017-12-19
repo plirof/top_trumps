cd9ae2581db298524145648cca6432af14f7f2e8
aaa <aaa@aaa.com>
2017-12-19 22:34:28 +0200

    test ajax 0001

---
+++ demo/fields.php
@@ -0,0 +1,26 @@
+<?php
+echo '
+{
+  "fields": [
+    {
+      "name": "Loyalty",
+      "comparison": ">"
+    },
+    {
+      "name": "Agility",
+      "comparison": ">"
+    },
+    {
+      "name": "Cuddliness",
+      "comparison": ">"
+    },
+    {
+      "name": "Appetite",
+      "comparison": "<"
+    }
+  ]
+}
+
+';
+
+?>
